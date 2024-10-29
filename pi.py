20a11f7aeed90e224c42d47bd4ec202f717620a6
Viner <145243810+Hasper1337@users.noreply.github.com>
2024-10-29 09:20:00 +0500

    Add files via upload

--- pi.py
+++ pi.py
@@ -1,13 +1,18 @@
+# формула Бейлия-Борвейна-Плаффа
 def pi_digits():
     q, r, t, k, n, l = 1, 0, 1, 1, 3, 3
     while True:
         if (4 * q + r - t) < (n * t):
-            yield (n/n**2) + n ##Условие вывода суммы отношения цифры на её квадрат и самой цифры
+            yield n
             q, r, t, k, n, l = 10 * q, 10 * (r - n * t), t, k, (10 * (3 * q + r)) // t - 10 * n, l
         else:
-            q, r, t, k, n, l = q * k, (2 * q + r) * l, t * l, k + 1, (q * (7 * k + 2) + r * l) // (t * l), l+2
+            q, r, t, k, n, l = q * k, (2 * q + r) * l, t * l, k + 1, (q * (7 * k + 2) + r * l) // (t * l), l + 2
 
 
-digits = pi_digits()
-for i in range(10):
-    print(next(digits))
+digits_generator = pi_digits()
+digits = [next(digits_generator) for _ in range(10)]
+
+values = list(map(lambda x: (x / x ** 2 if x != 0 else 0) + x, digits))  # выполнение задания
+
+for digit, value in zip(digits, values):
+    print(f"Цифра: {digit}, Вычисленное значение: {value}")
